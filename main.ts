9e6c58b47b6c950b8fbb05494173cf53d60d91f1
Quentin Bernet <quentin.bernet@bluewin.ch>
2025-01-16 22:04:07 +0100

    Rename app in one more spot

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface EveryDayCalendarSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: EveryDayCalendarSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class EveryDayCalendar extends Plugin {
+	settings: EveryDayCalendarSettings;
 
 	async onload() {
 		await this.loadSettings();
