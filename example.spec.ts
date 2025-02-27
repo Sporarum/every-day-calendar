37a5966142cacf1f8636a27b219ae0dfe05381a7
Quentin Bernet <quentin.bernet@bluewin.ch>
2025-02-27 16:45:37 +0100

    Add more timezones to tests

--- example.spec.ts
+++ example.spec.ts
@@ -1,24 +1,24 @@
 //import EveryDayCalendar from "./main";
 import { calculateDates } from "./dateCalculations";
 
-describe('EveryDayCalendar Tests', () => {
+describe('calculateDates', () => {
     test("should be importable", () => {
         expect(calculateDates).toBeTruthy();
     });
 
-    test("calculateDates is called as many times as there are days (non-leap year)", () => {
+    test("is called as many times as there are days (non-leap year)", () => {
         const mockFromDays = jest.fn()
         calculateDates(2025, mockFromDays)
         expect(mockFromDays).toHaveBeenCalledTimes(365);
     });
 
-    test("calculateDates is called as many times as there are days (leap year)", () => {
+    test("is called as many times as there are days (leap year)", () => {
         const mockFromDays = jest.fn()
         calculateDates(2024, mockFromDays)
         expect(mockFromDays).toHaveBeenCalledTimes(366);
     });
 
-    test("calculateDates returns the correct month names", () => {
+    test("returns the correct month names", () => {
         const mockFromDays = jest.fn()
         const months = calculateDates(2025, mockFromDays)
         const monthNames = months.map(m => m.name)
